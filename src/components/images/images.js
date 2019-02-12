080265614dae8865b2da5da66afa71c4336af265
oktavi <oktavisilitonga14@gmail.com>
2019-02-12 15:17:54 +1100

    update infokurs

--- src/components/images/images.js
+++ src/components/images/images.js
@@ -3,7 +3,7 @@ import React from 'react'
 const images =  (props) => {
   return (
     <div>
-        <img src={props.image} alt="gambar" />
+        <img src={props.image} alt="gambar" width="100%" />
     </div>
   )
 }
--- src/containers/InfoKurs/InfoKurs.js
+++ src/containers/InfoKurs/InfoKurs.js
@@ -2,48 +2,77 @@ import React, { Component } from 'react'
 import Image from '../../components/images/images';
 import BannerInfokurs from '../../assets/images/banner-info-kurs.jpg'
 import '../../containers/InfoKurs/InfoKurs.css';
+import { Link } from 'react-router-dom'
 
 class InfoKurs extends Component {
   render() {
     return (
-      <div >
+      <div>
         <Image image={BannerInfokurs} />
         <div className="heading">
           <div className="container">
-            <h1>Info Kurs</h1>
+            <div className="row col-lg-10">
+              <Link to="login">Beranda</Link>
+            </div>
           </div>
         </div>
         <div class="container">
-          <div class="row">
+          <div class="row col-lg-10" >
+            <h4>Infomasi Kurs</h4>
             <table class="table table-bordered">
               <thead>
                 <tr>
-                  <th>Mata Uang</th>
-                  <th>Bank Note</th>
-                  <th>TT(Terhadap Rp)</th>
+                  <th className="bg-green" rowspan="2">Mata Uang</th>
+                  <th className="bg-blue-grey" colspan="2">Bank Note</th>
+                  <th className="bg-blue-grey"colspan="2">TT(Terhadap Rp)</th>
+                </tr>
+                <tr>
+                  <th className="bg-blue-grey">Beli</th>
+                  <th className="bg-blue-grey">Jual</th>
+                  <th className="bg-blue-grey">Beli</th>
+                  <th className="bg-blue-grey">Jual</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>AUD</td>
-                  <td>9,761</td>
-                  <td>10,153</td>
+                  <td>9,776.50</td>
+                  <td>10,153.35</td>
+                  <td>9,776.50</td>
+                  <td>10,138.35</td>
                 </tr>
                 <tr>
                   <td>CAD</td>
                   <td>-</td>
                   <td>-</td>
+                  <td>10,408.20</td>
+                  <td>10,783.65</td>
                 </tr>
                 <tr>
                   <td>CHF</td>
-                  <td>13,797</td>
-                  <td>14,276</td>
+                  <td>13,797.50</td>
+                  <td>14,276.15</td>
+                  <td>13,812.50</td>
+                  <td>14,261.15</td>
+                </tr>
+                <tr>
+                  <td>DKK</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>2,057.70</td>
+                  <td>2,203.00</td>
+                </tr>
+                <tr>
+                  <td>EUR</td>
+                  <td>15,598.65</td>
+                  <td>16,196.90</td>
+                  <td>15,613.65</td>
+                  <td>16,181.90</td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
-
 
         <div className="heading box">
           <div className="container">
