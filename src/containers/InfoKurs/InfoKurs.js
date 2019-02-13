ddc5abeaa334824962ee532b2f1892b1d3ae1f5e
oktavi <oktavisilitonga14@gmail.com>
2019-02-14 09:25:00 +1100

    title images

--- src/containers/InfoKurs/InfoKurs.js
+++ src/containers/InfoKurs/InfoKurs.js
@@ -8,14 +8,16 @@ class InfoKurs extends Component {
   render() {
     return (
       <div>
-          <Image image={BannerInfokurs}/>
-          {/* <div className="imgTitle">
+        <div className="bgText">
+          <Image image={BannerInfokurs} />
+          <div className="imgTitle">
             <h2> Informasi Kurs Valuta Asing</h2>
-          </div> */}
+          </div>
+        </div>
         <div className="heading">
           <div className="container">
             <div className="row col-lg-10">
-              <Link to="/">Beranda</Link> 
+              <Link to="/">Beranda</Link>
             </div>
           </div>
         </div>
