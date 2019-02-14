888ebe6f67b9d033c633b8045d6a0a1ce0d81ce8
iivwijaya <iiv.wijaya@gmail.com>
2019-02-14 14:55:44 +0700

    aaaaaaa

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import { config as i18nextConfig } from "./translations";
+import { connect } from 'react-redux'
 import i18next from "i18next";
 
+// import * as action from '../src/store/action/index'
 import Login from './containers/Login/Login'
 import InfoKurs from './containers/InfoKurs/InfoKurs'
 import Header from './components/header/Header'
 import Layout from './components/hoc/layout/layout'
-import Beranda from './containers//Beranda/Beranda'
+import Beranda from './containers/Beranda/Beranda'
 import Footer from './components/footer/footer'
 import './App.css';
 
@@ -17,8 +19,16 @@ i18next.init(i18nextConfig);
 class App extends Component {
   componentDidMount() {
    document.getElementsByTagName('html', 'body')[0].setAttribute('style', 'margin: 0; overflow-x: hidden')
+  console.log(this.props)
   }
   render() {
+
+    let removeFooter
+      let page = this.props.page;
+      if(page !== 'Beranda'){
+        removeFooter = <Footer/>
+      }
+
     return (
       <I18nextProvider i18n={i18next}>
         <Layout>
@@ -30,11 +40,18 @@ class App extends Component {
               <Route path="/beranda" component={Beranda} />
             </Switch>
           </div>
-          <Footer />
+          {removeFooter}
         </Layout>
       </I18nextProvider>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state.head.page,'----------')
+  return{
+    page: state.head.page
+  }
+}
+
+export default connect(mapStateToProps,null)(App);
--- src/containers/Beranda/Beranda.js
+++ src/containers/Beranda/Beranda.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux'
+
+import * as action from '../../store/action/index'
 import './Beranda.css';
 
-export default class Beranda extends Component {
+class Beranda extends Component {
   
   // toggleSidebar() {
   //   document.getElementById("sidebar").classList.toggle('active');
   // }
   
+  componentDidMount(){
+    let page = this.props.location.pathname
+    this.props.changeTitle(page)
+    console.log(this.props.location.pathname)
+  }
+
   render() {
     return (
-      <div class="sidebar">
-        {/* <div class="toggle-btn" 
+      <div>
+       <div class="sidebar">
+         <div class="toggle-btn" 
         onClick={this.toggleSidebar}
         >
         <span></span>
         <span></span>
         <span></span>
-        </div> */}
+        </div>
         <ul>
           <li><Link to='#'>Beranda</Link></li>
           <li><Link to='#'>Rekening Saya</Link></li>
           <li><Link to='#'>Transfer</Link></li>
         </ul>
        </div>
-    )
+      </div>
+     )
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeTitle: (page) => dispatch(action.changeTitle(page))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Beranda)
