2b0667cd57072d3e80ca2e11f6186fb250c94ec4
iivwijaya <iiv.wijaya@gmail.com>
2019-02-18 10:49:54 +0700

    Mengubah state

--- src/components/formLogin/formLogin.js
+++ src/components/formLogin/formLogin.js
@@ -4,6 +4,29 @@ import { withTranslation } from 'react-i18next';
 import './formLogin.css'
 
 class formLogin extends Component {
+
+  state = {
+    newEmail: '',
+    newPassword: ''
+  }
+
+  // Event when value change
+  inputEmail = (event) => {
+    let newEmail = event.target.value.toString()
+    console.log(newEmail,'Email');
+    this.setState({
+      newEmail: newEmail
+    })
+  }
+
+  inputPassword = (event) => {
+    let newPassword = event.target.value.toString()
+    console.log(newPassword,'Pass');
+    this.setState({
+      newPassword: newPassword
+    })
+  }
+
   render() {
     const { t } = this.props;
     return (
@@ -12,11 +35,21 @@ class formLogin extends Component {
         <Form>
             <FormGroup>
             <Label for="exampleEmail">Email</Label>
-            <Input type="email" name="email" id="exampleEmail" placeholder="enter email" />
+            <Input 
+              type="email" 
+              name="email" 
+              id="exampleEmail" 
+              placeholder="enter email" 
+              onChange={(event) => this.inputEmail(event)}/>
             </FormGroup>
             <FormGroup>
             <Label for="examplePassword">Password</Label>
-            <Input type="password" name="password" id="examplePassword" placeholder="enter password" />
+            <Input 
+              type="password" 
+              name="password" 
+              id="examplePassword" 
+              placeholder="enter password"
+              onChange={(event) => this.inputPassword(event)} />
             </FormGroup>
             <Button color="success" block>Submit</Button>
       </Form>
